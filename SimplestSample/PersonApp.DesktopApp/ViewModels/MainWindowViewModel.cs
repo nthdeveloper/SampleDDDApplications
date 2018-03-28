1c7e17dc357743fdd2ea773095205c3f0b03b5b9
Mustafa Kok <mustafakok@gmail.com>
2018-03-28 12:49:43 +0300

    Added, refresh and remove functions.

--- SimplestSample/PersonApp.DesktopApp/ViewModels/MainWindowViewModel.cs
+++ SimplestSample/PersonApp.DesktopApp/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using MedicalRecorder.Commands;
 using PersonApp.Entities;
 using PersonApp.Services;
 
@@ -15,6 +17,12 @@ namespace PersonApp.DesktopApp.ViewModels
         Person m_SelectedPerson;
 
         public ObservableCollection<Person> Persons { get; private set; }
+
+        public ICommand RefreshCommand { get; private set; }
+
+        public ICommand AddCommand { get; private set; }
+
+        public ICommand RemoveCommand { get; private set; }        
 
         public Person SelectedPerson
         {
@@ -30,7 +38,36 @@ namespace PersonApp.DesktopApp.ViewModels
         {
             m_PersonService = personService;
 
-            this.Persons = new ObservableCollection<Person>(m_PersonService.GetAll());
+            this.Persons = new ObservableCollection<Person>();
+
+            this.RefreshCommand = new DelegateCommand<object>(executeRefreshCommand, null, "Refresh");
+            this.AddCommand = new DelegateCommand<object>(executeAddCommand, null, "Add");
+            this.RemoveCommand = new DelegateCommand<object>(executeRemoveCommand, null, "Remove");
+
+            executeRefreshCommand(null);
         }
+
+        private void executeRefreshCommand(object arg)
+        {
+            this.Persons.Clear();
+
+            var _allPersons = m_PersonService.GetAll();
+            foreach (var person in _allPersons)
+                this.Persons.Add(person);
+        }
+
+        private void executeAddCommand(object arg)
+        {
+
+        }
+
+        private void executeRemoveCommand(object arg)
+        {
+            if(m_SelectedPerson != null)
+            {
+                if (m_PersonService.Remove(m_SelectedPerson.Id))
+                    Persons.Remove(m_SelectedPerson);
+            }
+        }              
     }
 }
